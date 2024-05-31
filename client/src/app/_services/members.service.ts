240ea2ddd56b484124729ced6e46d9cddfb7517e
davidPope1 <david_1565@yahoo.com>
2024-05-31 12:11:36 +0300

    Added functionality to persist changes when editing profile to the API server, Angular route guard when leaving page with unsaved changes, loading indicator to client app and stored data in a service rather of making http calls every time

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 
 import { Member } from '../_modles/member';
 import { environment } from 'src/environments/environment';
+import { map, of } from 'rxjs';
 
 
 @Injectable({
@@ -10,16 +11,32 @@ import { environment } from 'src/environments/environment';
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+  members: Member[] = [];
 
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users')
+    if (this.members.length > 0) return of(this.members);
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+    )
   }
 
   getMember(username: string) {
+    const member = this.members.find(x => x.userName == username);
+    if (member) return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
-
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = {...this.members[index], ...member}
+      })
+    )
+  }
 }
