7054c193e353e27e5b2dd47cf0d94b3a4298d46e
davidPope1 <david_1565@yahoo.com>
2024-06-03 15:12:26 +0300

    Added functinallity for adding photos and removing photos and setting a photo to be the main one of the user

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -49,8 +49,9 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(user 
-                => user.UserName == loginDto.UserName);
+            var user = await _context.Users
+                .Include(p => p.Photos)
+                .SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
 
             if (user == null)
                 return Unauthorized("Invalid username");
@@ -68,7 +69,8 @@ namespace API.Controllers
             return new UserDto
             {
                 UserName = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
